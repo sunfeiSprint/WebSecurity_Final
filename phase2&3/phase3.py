df742be6de200862975b7bf8383a658289359490
sunfeiXjoyin <afei01234@gmail.com>
2015-04-18 15:32:37 +0800

    add filter for sensitive form

--- phase2&3/phase3.py
+++ phase2&3/phase3.py
@@ -3,11 +3,21 @@ __author__ = 'Sun Fei'
 import json
 import requests
 import parameter
+import config
 from loginform import fill_login_form
 from form import Form
 from pprint import pprint
 
+negKeywords = config.negKeywords
 
+def checkStringContainKey(testString,keyWords):
+    for word in negKeywords:
+
+        if word in testString:
+            return True   
+    return False
+
+#pprint(checkStringContainKey(testString,negKeywords))
 
 with open('phase1_final.json') as data_file:
     data = json.load(data_file)
@@ -32,26 +42,26 @@ with open('phase1_final.json') as data_file:
         for url in data[0]:
             formDetails = data[0][url]
             action = formDetails["action"]
-            if formDetails["method"].lower() == "get":# form is a get form, it cannot contain a token, always vulnerable for CSRF
-                ''#pprint("get form "+formDetails["action"]  +" is vulnerable to CSRF")
-                ###try to eliminate non-sensitive-data?###
+            if checkStringContainKey(action,negKeywords)==False:#check the Negative keywords to filter out non-sensitive data
+                if formDetails["method"].lower() == "get":# form is a get form, it cannot contain a token, always vulnerable for CSRF
+                    ''#pprint("get form "+formDetails["action"]  +" is vulnerable to CSRF")
+                    ###try to eliminate non-sensitive-data?###
 
-            elif formDetails["method"].lower() == "post":# form is a post form, check for CSRF
-                csrfForm = Form(url,formDetails)
-                #First, we send a valid request.
-                valid_parameters = dict(csrfForm.fill_entries())
-                ##pprint(valid_parameters)
-                r = client.post(action,valid_parameters)
+                elif formDetails["method"].lower() == "post":# form is a post form, check for CSRF
+                    csrfForm = Form(url,formDetails)
+                    #First, we send a valid request.
+                    valid_parameters = dict(csrfForm.fill_entries())
+                    ##pprint(valid_parameters)
+                    r = client.post(action,valid_parameters)
 
-                # Now, we suppress everything that looks like a token.
-                broken_parameters = dict(csrfForm.fill_entries("hidden"))
-                r = client.post(action,broken_parameters)
-                #content,code = csrfForm.send(action,broken_parameters,"post")
-                if r.status_code == 200:
-                    formDetails["url"] = url 
-                    jsonform.append(formDetails)
-                    #pprint("post form "+csrfForm.formdata["action"] +  " is vulnerable to CSRF")
+                    # Now, we suppress everything that looks like a token.
+                    broken_parameters = dict(csrfForm.fill_entries("hidden"))
+                    r = client.post(action,broken_parameters)
+                    #content,code = csrfForm.send(action,broken_parameters,"post")
+                    if r.status_code == 200:
+                        formDetails["url"] = url 
+                        jsonform.append(formDetails)
+                        #pprint("post form "+csrfForm.formdata["action"] +  " is vulnerable to CSRF")
 
 with open("phase3.json",'w') as outfile:
     json.dump(jsonform,outfile,indent=4)
-
