a8076e640179514442288dcd5a64067946819d44
sunfeiXjoyin <afei01234@gmail.com>
2015-04-18 12:30:46 +0800

    modify phase3

--- phase2&3/phase3.py
+++ phase2&3/phase3.py
@@ -8,42 +8,50 @@ from form import Form
 from pprint import pprint
 
 
-with open('phase1_sample.json') as data_file:
+
+with open('phase1_final.json') as data_file:
     data = json.load(data_file)
 
 
-client = requests.session()
+    client = requests.Session()
+    start_urls = parameter.login_urls
+    login_user = parameter.username
+    login_pass = parameter.password
 
-start_urls = parameter.login_urls
-login_user = parameter.username
-login_pass = parameter.password
+    response = client.get(start_urls[0],verify=False)
+    args, url, method = fill_login_form(response.url, response.content, login_user, login_pass)
 
-response = client.get(start_urls)
-args, url, method = fill_login_form(response.url, response.body, login_user, login_pass)
-loginResponse = client.post(url, data=args, headers=dict(Referer=start_urls))
+    loginResponse = client.post(url, data=args, headers=dict(Referer=start_urls))
+    pprint(loginResponse)
+    jsonform = []
+    if "ERROR: Invalid username" in response.content:
+        pprint("Login failed")
+    else: 
+        pprint("login successful")
 
-if "ERROR: Invalid username" in response.body:
-    pprint("Login failed")
-else: #login successful
-    for url in data[0]:
-        #pprint(url)
-        formDetails = data[0][url]
-        action = formDetails["action"]
-        if formDetails["method"].lower() == "get":# form is a get form, it cannot contain a token, always vulnerable for CSRF
-            pprint("get form "+formDetails["action"]  +" is vulnerable to CSRF")
-            ###try to eliminate non-sensitive-data?###
+        for url in data[0]:
+            formDetails = data[0][url]
+            action = formDetails["action"]
+            if formDetails["method"].lower() == "get":# form is a get form, it cannot contain a token, always vulnerable for CSRF
+                ''#pprint("get form "+formDetails["action"]  +" is vulnerable to CSRF")
+                ###try to eliminate non-sensitive-data?###
 
-        elif formDetails["method"].lower() == "post":# form is a post form, check for CSRF
-            csrfForm = Form(url,formDetails)
-            #First, we send a valid request.
-            valid_parameters = dict(csrfForm.fill_entries())
-            ####pprint(valid_parameters)
-            r = client.post(action,valid_parameters)
+            elif formDetails["method"].lower() == "post":# form is a post form, check for CSRF
+                csrfForm = Form(url,formDetails)
+                #First, we send a valid request.
+                valid_parameters = dict(csrfForm.fill_entries())
+                ##pprint(valid_parameters)
+                r = client.post(action,valid_parameters)
 
-            # Now, we suppress everything that looks like a token.
-            broken_parameters = dict(csrfForm.fill_entries("hidden"))
-            #content,code = csrfForm.send(action,broken_parameters,"post")
-            if r.status_code == 200:
-                pprint("post form "+csrfForm.formdata["action"] +  " is vulnerable to CSRF")
+                # Now, we suppress everything that looks like a token.
+                broken_parameters = dict(csrfForm.fill_entries("hidden"))
+                r = client.post(action,broken_parameters)
+                #content,code = csrfForm.send(action,broken_parameters,"post")
+                if r.status_code == 200:
+                    formDetails["url"] = url 
+                    jsonform.append(formDetails)
+                    #pprint("post form "+csrfForm.formdata["action"] +  " is vulnerable to CSRF")
 
+with open("phase3.json",'w') as outfile:
+    json.dump(jsonform,outfile,indent=4)
 
