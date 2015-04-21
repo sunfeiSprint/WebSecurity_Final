c120217488e8191a354e18d945b5a0326aa199ed
ZillDing <zilldzy@gmail.com>
2015-04-21 20:06:26 +0800

    a) fix phase4 generation scripts typo b) clean up

--- start.sh
+++ start.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-##You need to modify the config.json to configure loginflag(whether login is needed ) login_url ,start_url ,username ,password
-##Before you run this script
+# Before you run this script
+# You need to modify the config.json to configure:
+# - loginflag(whether login is needed )
+# - login_url
+# - start_url
+# - username
+# - password
 
+# phase 1
 cd ./phase1
 sh phase1.sh
+
+# phase 2 & 3
 cd ../phase2_3
 python phase2.py
 python phase3.py
 
-#cd ../phase4
-#python generate_exploit.py -c ../config/config.json -i ../output/phase3.json -o ../output/
-
+# phase 4
+cd ../phase4
+python generate_exploit.py -c ../config/config.json -i ../output/phase3_output.json -o ../exploit_scripts
